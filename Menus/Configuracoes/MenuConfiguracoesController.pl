1d0e402f98a03ee2e4b797c690bd9dc041998a25
kpzinnm <gabriel.santos.alves@ccc.ufcg.edu.br>
2024-05-04 10:24:22 -0300

    Mergeando dev

--- Menus/Configuracoes/MenuConfiguracoesController.pl
+++ Menus/Configuracoes/MenuConfiguracoesController.pl
@@ -17,6 +17,7 @@ starMenuConfiguracoes :-
 optionsStartMenu(UserChoice) :-
     (UserChoice == "V" ; UserChoice ==  "v") -> startMenu ;  
     (UserChoice == "F" ; UserChoice ==  "f") -> adicionarFilme ; 
+    (UserChoice == "S" ; UserChoice ==  "s") -> adicionarSessao ;
     writeln("\nOpção Inválida!"),
     sleep(0.7),
     startMenu.
@@ -32,5 +33,31 @@ adicionarFilme :-
     createFilme("0", Titulo, Duracao, Genero, Filme),
     saveFilme(Filme),
     starMenuConfiguracoes.
+
+adicionarSessao :-
+    printMatrix("./Interfaces/Configuracoes/MenuCadastroSessoe.txt"),
+    write("Digite o Identificador do filme:"),
+    flush_output,
+    read_line_to_string(user_input, IdFilme),
+    isFilmeValido(IdFilme, Bool),
+    (Bool ->
+        write("Digite o horario no formato (<hora>, <minutos>): "),
+        flush_output,
+        read(Horario),
+        write("Informa a capacidade: "),
+        flush_output,
+        read_line_to_string(user_input, Capacidade),
+        write("Informe o ID da sala: "),
+        flush_output
+        
+        ;
+        write("Filme não registrado"),
+        sleep(1.2),
+        starMenuConfiguracoes
+    ).
+
+    
+            
+            
 
 
